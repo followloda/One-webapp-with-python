3bcca0763d86d71d95b1be656c9a0c54867a2f9c
ghostloda <78798447@qq.com>
2017-12-07 21:37:31 +0800

    使用docker搭建web服务

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_file
+import os
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello():
+    return "Hello World from Flask"
+
+
+@app.route("/")
+def main():
+    index_path = os.path.join(app.static_folder, 'index.html')
+    return send_file(index_path)
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
