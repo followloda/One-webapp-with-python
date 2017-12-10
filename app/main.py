875a8dc055849cc3108e4b8b36e7fc43524545b8
ghostloda <78798447@qq.com>
2017-12-10 19:27:13 +0800

    应用结构调整

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ from app.lifeboat import app
 
 if __name__ == "__main__":
     #Only for debugging while developing
-    #app.run(host='0.0.0.0', debug=True, port=80)
-    app.run(host='127.0.0.1', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=80)
+
